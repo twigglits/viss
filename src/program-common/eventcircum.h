92fbf2e10acb9e45c869941376f540d46a20e473
twigglits <jnaude60512@gmail.com>
2025-06-14 19:00:14 +0200

    adding circum event header file. stripped down the the max

---
+++ src/program-common/eventcircum.h
@@ -0,0 +1,36 @@
+#ifndef EVENTCIRCUM_H
+
+#define EVENTCIRCUM_H
+
+#include "simpactevent.h"
+
+// We instantiate classes when we de-reference them with (&) 
+// we do not instantiate classes when we pass them as a pointer (*). 
+class ConfigSettings;
+class ProbabilityDistribution;
+class ConfigWriter;
+
+class EventCircum : public SimpactEvent
+{
+public:
+	EventCircum(Person *pPerson);
+	~EventCircum();
+
+	std::string getDescription(double tNow) const;
+	void writeLogs(const SimpactPopulation &pop, double tNow) const;
+	void fire(Algorithm *pAlgorithm, State *pState, double t);
+
+	static void processConfig(ConfigSettings &config, GslRandomNumberGenerator *pRndGen);
+	static void obtainConfig(ConfigWriter &config);
+private:
+	bool isEligibleForTreatment(double t);
+	bool isWillingToStartTreatment(double t, GslRandomNumberGenerator *pRndGen);
+    double getNewInternalTimeDifference(GslRandomNumberGenerator *pRndGen, const State *pState);
+
+	bool m_CircumEnabled; 
+	double m_CircumThreshold;
+
+    static ProbabilityDistribution *s_CircumcProbDist;
+};
+
+#endif //EVENTCIRCUM_H
