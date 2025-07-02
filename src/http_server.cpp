77af54c0b46db057a00f486f6fcb43edee78b2d2
twigglits <jnaude60512@gmail.com>
2025-07-02 16:34:23 +0200

    adding stuff to make backend callable like an API

---
+++ src/http_server.cpp
@@ -0,0 +1,29 @@
+#include "../external/crow_all.h"
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+#include "../external/crow_all.h"
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int main() {
+    crow::SimpleApp app;
+    std::cout << "[viss-api] Crow REST API server starting on port 8000...\n";
+
+    CROW_ROUTE(app, "/run_simulation")([](const crow::request& req){
+        std::cout << "[viss-api] /run_simulation endpoint called. Running simulation...\n";
+        int ret = std::system("./build/viss-release test_config1.txt 0 opt -o > output.txt");
+        std::ifstream t("output.txt");
+        std::string output((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+        std::cout << "[viss-api] Simulation complete. Returning output.\n";
+        return crow::response(output);
+    });
+
+    app.port(8000).multithreaded().run();
+    std::cout << "[viss-api] Crow REST API server stopped.\n";
+    return 0;
+}
+
